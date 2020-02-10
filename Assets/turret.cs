e21c426d5dea0355b27734039450c8cbd7512136
Alexander Schoel <alexexander.s@gmail.com>
2020-02-10 14:24:45 +0100

    First Commit

---
+++ Assets/turret.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour
+{
+    bool inradius = false;
+    public GameObject player;
+    public AnimationCurve myCurve;
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inradius)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-90, 0, 0), 3f);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inradius = true;
+            if (transform.rotation != Quaternion.LookRotation(other.transform.position - transform.position))
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(other.transform.position - transform.position), 3f);
+                Debug.Log("Help");
+
+            }
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inradius = false;
+    }
+}
