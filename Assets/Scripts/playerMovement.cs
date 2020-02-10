a5e0b2965dcd6edaaa314dddc4a42579bfa78dd7
KingSnailtan <alexexander.s@gmail.com>
2020-02-10 15:17:21 +0100

    h

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,7 +7,7 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //test
+        //testh
     }
 
     // Update is called once per frame
